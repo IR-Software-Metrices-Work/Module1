f3ec5ee918715a9d339586c14912ec564c030a80
Oralita <54722120+oat431@users.noreply.github.com>
2021-11-15 15:19:55 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import string
+import requests as requests
+from bs4 import BeautifulSoup
+import numpy as np
+
 
 def get_and_clean_data():
     data = pd.read_csv('software_developer_united_states_1971_20191023_1.csv')
@@ -18,3 +22,26 @@ def parse_job_description():
     cleaned_description = get_and_clean_data()
     cleaned_description = simple_tokenize(cleaned_description)
     return cleaned_description
+
+def count_python_mysql():
+    parsed_description = parse_job_description()
+    count_python = parsed_description.apply(lambda s: 'python' in s).sum()
+    count_mysql = parsed_description.apply(lambda s: 'mysql' in s).sum()
+    print('python: ' + str(count_python) + ' of ' + str(parsed_description.shape[0]))
+    print('mysql: ' + str(count_mysql) + ' of ' + str(parsed_description.shape[0]))
+
+def parse_db():
+    html_doc = requests.get("https://www.improgrammer.net/top-10-databases-should-learn-2015/").content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    all_db = [s.get_text() for s in soup.findAll('dt')]
+    db_list = [s.lower() for s in all_db]
+    db_list = [[x.strip() for x in s.split()] for s in db_list]
+    return db_list
+
+def after_python_lesson() :
+    parsed_db = parse_db()
+    parsed_description = parse_job_description()
+    with_python = [None] * len(parsed_db)
+    for i,db in enumerate(parse_db()) :
+        with_python[i] = parsed_description.apply(lambda s: np.all([x in s for x in db]) and 'python' in s).sum()
+        print(' '.join(db) + ' + python: ' + str(with_python[i]) + ' of ' + str(parsed_description.shape[0]))
