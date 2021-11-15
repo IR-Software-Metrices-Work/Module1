6a0ea9ec1289218c93e8b9d20806e1268f814353
Oralita <54722120+oat431@users.noreply.github.com>
2021-11-15 18:17:58 +0700

    [🎄 finish the handsout]

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests as requests
 from bs4 import BeautifulSoup
 import numpy as np
 
-
 def get_and_clean_data():
     data = pd.read_csv('software_developer_united_states_1971_20191023_1.csv')
     description = data['job_description']
@@ -38,10 +37,42 @@ def parse_db():
     db_list = [[x.strip() for x in s.split()] for s in db_list]
     return db_list
 
+def count_db():
+    parsed_db = parse_db()
+    parsed_description = parse_job_description()
+    counted_db = [None] * len(parsed_db)
+    for i, db in enumerate(parsed_db) :
+        counted_db[i] = parsed_description.apply(lambda s: np.all([x in s for x in db])).sum()
+        print(' '.join(db), ':', counted_db[i], 'of', parsed_description.shape[0])
+
 def after_python_lesson() :
     parsed_db = parse_db()
     parsed_description = parse_job_description()
     with_python = [None] * len(parsed_db)
-    for i,db in enumerate(parse_db()) :
+    for i, db in enumerate(parsed_db):
         with_python[i] = parsed_description.apply(lambda s: np.all([x in s for x in db]) and 'python' in s).sum()
         print(' '.join(db) + ' + python: ' + str(with_python[i]) + ' of ' + str(parsed_description.shape[0]))
+
+def to_percent(a,b):
+    return str(round((100/b) * a, 2))
+
+def insight_of_python_and_database() :
+    parsed_db = parse_db()
+    parsed_description = parse_job_description()
+    with_python = [None] * len(parsed_db)
+    counted_db = [None] * len(parsed_db)
+    for i, db in enumerate(parsed_db):
+        with_python[i] = parsed_description.apply(lambda s: np.all([x in s for x in db]) and 'python' in s).sum()
+        counted_db[i] = parsed_description.apply(lambda s: np.all([x in s for x in db])).sum()
+        percent = to_percent(with_python[i], counted_db[i])
+        print(' '.join(db) + ' + python: ' + str(with_python[i]) + ' of ' + str(counted_db[i]) + ' (' + percent + '%)')
+
+def all_lang() :
+    lang = [['java'], ['python'], ['c'], ['kotlin'], ['swift'], ['rust'], ['ruby'], ['scala'], ['julia'], ['lua']]
+    parsed_description = parse_job_description()
+    parsed_db = parse_db()
+    all_terms = lang + parsed_db
+    query_map = pd.DataFrame(parsed_description.apply(
+        lambda s: [1 if np.all([d in s for d in db]) else 0 for db in all_terms]).values.tolist(),
+        columns=[' '.join(d) for d in all_terms])
+    print(query_map)
